e4516545b10e49ef3bcc76dc156990518571b81f
Anthony Price <priceanthony588@gmail.com>
2024-01-30 15:11:13 -0500

    This is my new main C ++

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main() {
     std::cout << "Enter your name:";
     std::string name;
     std::getline(std::cin, name);
-    std::cout << "Hello " << name << "!" << std::endl;
+    std::cout << "Hello! " << name << "!" << std::endl;
 
 
     return 0;
